a656839d15d15cc5316301e921c07f1fbecda837
Zeezarh <ooyeniyiazizat@yahoo.com>
2022-10-13 09:46:51 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
-#!/bin/bash
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-   puts("\â€"Programming is like building a multilingual puzzle");
+   puts("\"Programming is like building a multilingual puzzle");
    return (0);
 
-}
+} 
