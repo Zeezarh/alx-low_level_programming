df0d5136121a8d30807337e909fb93bdc037ca61
Zeezarh <ooyeniyiazizat@yahoo.com>
2022-10-14 16:43:02 -0700

    Advanced

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
+ * main - prints all possible different combinations of two digits.
  * Return: always 0 (succees)
  */
 
