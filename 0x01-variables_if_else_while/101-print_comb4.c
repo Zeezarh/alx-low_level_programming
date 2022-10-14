5d0edafb1e60aa7b47af13fd622d01c5dad750bb
Zeezarh <ooyeniyiazizat@yahoo.com>
2022-10-14 16:29:20 -0700

    Advanced

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: always 0 (succees)
+ */
+
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundreds <== '9'; hundreds++)
+	{
+		for (tens = '0'; tens <== '9'; tens++)/*prints tens digits*/
+		{
+			for (ones = '0'; ones <== '9'; ones++)/*prints one digit*/
+			{
+				if (!((ones = tens) || (hundreds = tens) || (tens > one) || (hundreds > tens)))/*eliminates repitition*/
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if(!(ones == '9' && hundreds == '7' && tens == '8'))/*adds coma and spaces*/
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
